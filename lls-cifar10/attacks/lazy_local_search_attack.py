a8a2172a799f72d71cdebee1d43fde7e5206cc41
drillermoon <symoon11@snu.ac.kr>
2019-04-24 14:31:39 +0900

    edit cifar10

--- lls-cifar10/attacks/lazy_local_search_attack.py
+++ lls-cifar10/attacks/lazy_local_search_attack.py
@@ -1,250 +1,134 @@
+import cv2
 import tensorflow as tf
-import numpy as np
-import heapq
 import math
+import numpy as np
 import time
+import itertools
+
+from attacks.lazy_local_search_helper import LazyLocalSearchHelper
 
 
 class LazyLocalSearchAttack(object):
+  """Lazy Local Search Attack with Hierachical Method and Mini-batch Technique"""
+
   def __init__(self, model, args, **kwargs):
-    # Hyperparameter setting (general)
-    self.epsilon = args.epsilon
-    self.max_queries = args.max_queries
+    """Initialize attack method
+    
+    Args:
+      model: tensorflow model
+      args: arguments 
+    """
+    # Setting
     self.loss_func = args.loss_func
-
-    # Network setting
-    self.x_input = model.x_input
-    self.y_input = model.y_input
-  
-    logits = model.logits
-    self.preds = model.predictions
-    probs = tf.nn.softmax(logits)
-    batch_num = tf.range(0, limit=tf.shape(probs)[0])
-    indices = tf.stack([batch_num, self.y_input], axis=1)
-    ground_truth_probs = tf.gather_nd(params=probs, indices=indices)
-    top_2 = tf.nn.top_k(probs, k=2)
-    max_indices = tf.where(tf.equal(top_2.indices[:, 0], self.y_input), 
-      top_2.indices[:, 1], top_2.indices[:, 0])
-    max_indices = tf.stack([batch_num, max_indices], axis=1)
-    max_probs = tf.gather_nd(params=probs, indices=max_indices)
-    
-    if self.loss_func == 'xent':
-      self.losses = -tf.nn.sparse_softmax_cross_entropy_with_logits(
-        logits=logits, labels=self.y_input)
-    elif self.loss_func == 'cw':
-      self.losses = tf.log(ground_truth_probs+1e-10) - tf.log(max_probs+1e-10) 
-    
+    self.max_queries = args.max_queries
+    self.epsilon = args.epsilon
+    self.batch_size = args.batch_size
+    self.block_size = args.block_size
+    self.no_hier = args.no_hier
+    self.max_iters = args.max_iters
+         
+    # Load lazy local search helper
+    self.lazy_local_search = LazyLocalSearchHelper(model, args)
+ 
   def _perturb_image(self, image, noise):
+    """Perturb an image with a noise, and clip the value of the perturbed image into [0, 255] 
+    
+    Args:
+      image: numpy array with size [1, 32, 32, 3], original image
+      noise: numpy array with size [1, 32, 32, 3], noise
+
+    Returns:
+      adv_image: numpy array with size [1, 299, 299, 3], perturbed image
+    """
     adv_image = image + noise
-    adv_image = np.clip(adv_image, 0., 255.)
-    return adv_image   
-     
-  def _flip_noise(self, noise, w, h, c):
-    noise_new = np.copy(noise)
-    noise_new[0, w, h, c] *= -1
-    return noise_new
+    adv_image = np.clip(adv_image, 0, 255)
+    return adv_image
 
-  def perturb(self, image, label, sess):		
-    # Local variable
-    priority_queue = []
+  def _split_block(self, upper_left, lower_right, block_size):
+    """Split an image into blocks with block_size. Note that a block consists of 
+    [upper_left, lower_right, channel]
+
+    Args:
+      upper_left: [x, y], the coordinate of the upper left of an image
+      lower_right: [x, y], the coordinate of the lower right of an image
+      block_size: int, the size of a block
+
+    Return:
+      blocks: list, the set of blocks
+    """
+    blocks = []
+    xs = np.arange(upper_left[0], lower_right[0], block_size)
+    ys = np.arange(upper_left[1], lower_right[1], block_size)
+    for x, y in itertools.product(xs, ys):
+      for c in range(3):
+        blocks.append([[x, y], [x+block_size, y+block_size], c])
+    return blocks
+  
+  def perturb(self, image, label, index, sess):		
+    """Perturb an image with (or without) target label.
+    
+    Args:
+      image: numpy array with size [1, 32, 32, 3], original image
+      label: numpy array with size [1], the label of image (or target label)
+      index: int, the index of image
+      sess: tensorflow session
+
+    Returns:
+      adv_image: numpy array with size [1, 32, 32, 3], adversarial image
+      num_queries: int, query count
+      success: boolean, True if attack is successful
+    """
+    # Set random seed by index for the reproducibility
+    np.random.seed(index)
+    
+    # Local variables
     num_queries = 0
-    success = False
-    step = 0
+    block_size = self.block_size
+    upper_left = [0, 0]
+    lower_right = [32, 32]
     
-    adv_image = np.copy(image)
-    A = np.zeros_like(image, np.int32)
-    noise = -self.epsilon*np.ones_like(image, np.float32)
-
-    # Calculate current loss
-    image_batch = self._perturb_image(image, noise)
-    label_batch = np.copy(label)
-    losses, preds = sess.run([self.losses, self.preds], 
-      feed_dict={self.x_input: image_batch, self.y_input: label_batch})
-    num_queries += 1
-    curr_loss = losses[0]
-
-    if preds != label:
-      adv_image = self._perturb_image(image, noise)
-      return adv_image, num_queries, True
+    # Split image into blocks
+    blocks = self._split_block(upper_left, lower_right, block_size) 
     
+    # Initialize noise
+    noise = -self.epsilon*np.ones_like(image, dtype=np.int32)
+
+    # Prepare for batch
+    num_blocks = len(blocks)
+    batch_size = self.batch_size if self.batch_size > 0 else num_blocks
+    curr_order = np.random.permutation(num_blocks)
+
+    # Main loop
     while True:
-      # First forward passes
-      _, ws, hs, cs = np.where(A==0)
-      
-      batch_size = 100
-      num_batches = int(math.ceil(len(ws)/batch_size))   
-      
-      for ibatch in range(num_batches):
-        bstart = ibatch * batch_size
-        bend = min(bstart + batch_size, len(ws))
-        
-        image_batch = np.zeros([bend-bstart, 32, 32, 3], np.float32) 
-        noise_batch = np.zeros([bend-bstart, 32, 32, 3], np.float32)
-        label_batch = np.tile(label, bend-bstart)
-
-        for i, (w, h, c) in enumerate(zip(ws[bstart:bend], hs[bstart:bend], cs[bstart:bend])):
-          noise_batch[i:i+1, ...] = self._flip_noise(noise, w, h, c)
-          image_batch[i:i+1, ...] = self._perturb_image(image, noise_batch[i:i+1, ...])
-
-        losses, preds = sess.run([self.losses, self.preds],
-          feed_dict={self.x_input: image_batch, self.y_input: label_batch})
-        
-        # Early stopping
-        success_indices, = np.where(preds != label)
-        if len(success_indices) > 0:
-          noise = noise_batch[success_indices[0], ...]
-          num_queries += success_indices[0]+1
-          if num_queries > self.max_queries:
-            return adv_image, num_queries, False
-          adv_image = self._perturb_image(image, noise)
+      # Run batch
+      num_batches = int(math.ceil(num_blocks/batch_size))
+      for i in range(num_batches):
+        # Construct a mini-batch
+        bstart = i*batch_size
+        bend = min(bstart + batch_size, num_blocks)
+        blocks_batch = [blocks[curr_order[idx]] for idx in range(bstart, bend)]
+        # Run lazy local search
+        noise, queries, loss, success = self.lazy_local_search.perturb(
+          image, noise, label, sess, blocks_batch)
+        num_queries += queries
+        tf.logging.info("Block size: {}, batch: {}, loss: {:.4f}, num queries: {}".format(
+          block_size, i, loss, num_queries))
+        # If query count exceeds max queries, then return False
+        if num_queries > self.max_queries:
+          return adv_image, num_queries, False
+        # Update adversarial image  
+        adv_image = self._perturb_image(image, noise)
+        # If success, return True
+        if success:
           return adv_image, num_queries, True
-        num_queries += bend-bstart
-        
-        # Push into the priority queue
-        for i in range(bend-bstart):
-          w = ws[bstart+i]
-          h = hs[bstart+i]
-          c = cs[bstart+i]
-          margin = losses[i] - curr_loss
-          heapq.heappush(priority_queue, (margin, w, h, c))
       
-      # Pick the best element and perturb the image   
-      best_margin, best_w, best_h, best_c = heapq.heappop(priority_queue)
-      curr_loss += best_margin
-      noise = self._flip_noise(noise, best_w, best_h, best_c)
-      A[0, best_w, best_h, best_c] = 1
-      
-      # Add element into the set
-      while len(priority_queue) > 0:
-        # pick the best element
-        cand_margin, cand_w, cand_h, cand_c = heapq.heappop(priority_queue)
-       
-        # Re-evalulate the element
-        image_batch = self._perturb_image(
-          image, self._flip_noise(noise, cand_w, cand_h, cand_c))
-        label_batch = np.copy(label)
-        
-        losses, preds = sess.run([self.losses, self.preds],
-          feed_dict={self.x_input: image_batch, self.y_input: label_batch})
-        num_queries += 1
-        margin = losses[0]-curr_loss
-        
-        # If the cardinality has not changed, perturb the image
-        if len(priority_queue) == 0 or margin <= priority_queue[0][0]:
-          # If there is no element that has negative margin, then break
-          if margin >= 0:
-            break
-          # Perturb image  
-          curr_loss = losses[0]
-          noise = self._flip_noise(noise, cand_w, cand_h, cand_c)
-          A[0, cand_w, cand_h, cand_c] = 1
-          # Early stopping
-          if preds != label:
-            success = True
-            break
-
-        # If the cardinality has changed, push the element into the priority queue
-        else: 
-          heapq.heappush(priority_queue, (margin, cand_w, cand_h, cand_c))
-	    
-      tf.logging.info('Adding elements, step {}, loss: {:.4f}, num_queries: {}'.format(
-        step, curr_loss, num_queries))
-
-      if num_queries > self.max_queries:
-        return adv_image, num_queries, False
-      adv_image = self._perturb_image(image, noise)
-      if success:
-        return adv_image, num_queries, True
-       
-      priority_queue = []
-
-      # Now delete element
-      _, ws, hs, cs = np.where(A == 1)
-       
-      batch_size = 100
-      num_batches = int(math.ceil(len(ws)/batch_size))   
-      
-      for ibatch in range(num_batches):
-        bstart = ibatch * batch_size
-        bend = min(bstart + batch_size, len(ws))
-        
-        image_batch = np.zeros([bend-bstart, 32, 32, 3], np.float32) 
-        noise_batch = np.zeros([bend-bstart, 32, 32, 3], np.float32)
-        label_batch = np.tile(label, bend-bstart)
-
-        for i, (w, h, c) in enumerate(zip(ws[bstart:bend], hs[bstart:bend], cs[bstart:bend])):
-          noise_batch[i:i+1, ...] = self._flip_noise(noise, w, h, c)
-          image_batch[i:i+1, ...] = self._perturb_image(image, noise_batch[i:i+1, ...])
-
-        losses, preds = sess.run([self.losses, self.preds],
-          feed_dict={self.x_input: image_batch, self.y_input: label_batch})
-       
-        # Early stopping 
-        success_indices, = np.where(preds != label)
-        if len(success_indices) > 0:
-          noise = noise_batch[success_indices[0], ...]
-          num_queries += success_indices[0]+1
-          if num_queries > self.max_queries:
-            return adv_image, num_queries, False
-          adv_image = self._perturb_image(image, noise)
-          return adv_image, num_queries, True
-        num_queries += bend-bstart
-        
-        # Push into the priority queue
-        for i in range(bend-bstart):
-          w = ws[bstart+i]
-          h = hs[bstart+i]
-          c = cs[bstart+i]
-          margin = losses[i] - curr_loss
-          heapq.heappush(priority_queue, (margin, w, h, c))
-    
-      # Pick the best element and perturb the image   
-      best_margin, best_w, best_h, best_c = heapq.heappop(priority_queue)
-      curr_loss += best_margin
-      noise = self._flip_noise(noise, best_w, best_h, best_c)
-      A[0, best_w, best_h, best_c] = 0
-
-      # Add element into the set
-      while len(priority_queue) > 0:
-        # pick the best element
-        cand_margin, cand_w, cand_h, cand_c = heapq.heappop(priority_queue)
-        
-        # Re-evalulate the element
-        image_batch = self._perturb_image(
-          image, self._flip_noise(noise, cand_w, cand_h, cand_c))
-        label_batch = np.copy(label)
-        
-        losses, preds = sess.run([self.losses, self.preds],
-          feed_dict={self.x_input: image_batch, self.y_input: label_batch})
-        num_queries += 1
-        margin = losses[0]-curr_loss
-        
-        # If the cardinality has not changed, perturb the image
-        if len(priority_queue) == 0 or margin <= priority_queue[0][0]:
-          # If there is no element that has negative margin, then break
-          if margin >= 0:
-            break
-          # Perturb image  
-          curr_loss = losses[0]
-          noise = self._flip_noise(noise, cand_w, cand_h, cand_c)
-          A[0, cand_w, cand_h, cand_c] = 0
-          # Early stopping
-          if preds != label:
-            success = True
-            break
-
-        # If the cardinality has changed, push the element into the priority queue
-        else: 
-          heapq.heappush(priority_queue, (margin, cand_w, cand_h, cand_c))
-      
-      tf.logging.info("Deleting elements, step {}, loss: {:.4f}, num_queries: {}".format(
-        step, curr_loss, num_queries)) 
-      if num_queries > self.max_queries:
-        return adv_image, num_queries, False
-      adv_image = self._perturb_image(image, noise)
-      if success:
-        return adv_image, num_queries, True
-
-      priority_queue = []
-      step += 1
-
+      # If block size >= 2, then split blocks
+      if not self.no_hier and block_size >= 2:
+        block_size //= 2
+        blocks = self._split_block(upper_left, lower_right, block_size)
+        num_blocks = len(blocks)
+        batch_size = self.batch_size if self.batch_size > 0 else num_blocks
+        curr_order = np.random.permutation(num_blocks)
+      # Otherwise, shuffle the order of batches
+      else:
+        curr_order = np.random.permutation(num_blocks)
--- lls-cifar10/attacks/lazy_local_search_helper.py
+++ lls-cifar10/attacks/lazy_local_search_helper.py
@@ -3,41 +3,118 @@ import tensorflow as tf
 import numpy as np
 import heapq
 import math
+import sys
 import time
 
 
 class LazyLocalSearchHelper(object):
-  def __init__(self, model, loss_func, epsilon, max_iters, **kwargs):
-    # Hyperparameter Setting
-    self.epsilon = epsilon
-    self.max_iters = max_iters
+  """Lazy Local Search Helper
+  Note that since heapq library only supports min heap, 
+  we flip the sign of loss function.
+  """
+
+  def __init__(self, model, args, **kwargs):
+    """Initialize lazy local search helper
     
+    Args:
+      model: tensorflow model
+      loss_func: str, the type of loss function
+      epsilon: int, the maximum perturbation of pixel value
+    """
+    # Hyperparameter Setting
+    self.epsilon = args.epsilon
+    self.max_iters = args.max_iters
+    self.targeted = args.targeted
+    self.loss_func = args.loss_func
+
     # Network Setting
     self.model = model
     self.x_input = model.x_input
     self.y_input = model.y_input
     self.logits = self.model.logits
-    self.losses = -tf.nn.sparse_softmax_cross_entropy_with_logits(
-      logits=self.logits, labels=self.model.y_input)
     self.preds = model.predictions
-     
+    
+    probs = tf.nn.softmax(self.logits)
+    batch_num = tf.range(0, limit=tf.shape(probs)[0]) 
+    indices = tf.stack([batch_num, self.y_input], axis=1)
+    ground_truth_probs = tf.gather_nd(params=probs, indices=indices)
+    top_2 = tf.nn.top_k(probs, k=2)
+    max_indices = tf.where(tf.equal(top_2.indices[:, 0], self.y_input), top_2.indices[:, 1], top_2.indices[:, 0])
+    max_indices = tf.stack([batch_num, max_indices], axis=1)
+    max_probs = tf.gather_nd(params=probs, indices=max_indices)
+    
+    # Uncomment if you want to get a deterministic result
+    if self.targeted:
+      if self.loss_func == 'xent':
+        self.losses = tf.nn.sparse_softmax_cross_entropy_with_logits(
+          logits=self.logits, labels=self.model.y_input)
+        #self.losses = -tf.log(ground_truth_probs+1e-10)
+      elif self.loss_func == 'cw':
+        self.losses = tf.log(max_probs+1e-10) - tf.log(ground_truth_probs+1e-10)
+      else:
+        tf.logging.info('Loss function must be xent or cw')
+        sys.exit()
+    else:
+      if self.loss_func == 'xent':
+        self.losses = -tf.nn.sparse_softmax_cross_entropy_with_logits(
+          logits=self.logits, labels=self.model.y_input)
+        #self.losses = tf.log(ground_truth_probs+1e-10)
+      elif self.loss_func == 'cw':
+        self.losses = tf.log(ground_truth_probs+1e-10) - tf.log(max_probs+1e-10)
+      else:
+        tf.logging.info('Loss function must be xent or cw')
+        sys.exit()
+
   def _perturb_image(self, image, noise):
+    """Perturb an image with a noise, and clip the value of the perturbed image into [0, 255]
+    
+    Args:
+      image: numpy array with size [1, 32, 32, 3], original image
+      noise: numpy array with size [1, 32, 32, 3]
+    
+    Returns:
+      adv_image: numpy array with size [1, 32, 32, 3], perturbed image 
+    """
+
     adv_image = image + noise
     adv_image = np.clip(adv_image, 0, 255)
     return adv_image 
 
   def _flip_noise(self, noise, block):
+    """Filp the sign of perturbation on a block
+    Args:
+      noise: numpy array with size [1, 32, 32, 3]
+      block: [upper_left, lower_right, channel]
+  
+    Returns:
+      noise_new: numpy array with size [1, 32, 32, 3], updated noise
+    """
     noise_new = np.copy(noise)
     upper_left, lower_right, channel = block 
     noise_new[0, upper_left[0]:lower_right[0], upper_left[1]:lower_right[1], channel] *= -1
     return noise_new
 
   def perturb(self, image, noise, label, sess, blocks):		
+    """Update noise with lazy local search
+    
+    Args:
+      image: numpy array with size [1, 32, 32, 3], original image
+      noise: numpy array with size [1, 32, 32, 3]
+      label: numpy array with size [1], the label of image (or target label)
+      sess: tensorflow session
+      blocks: list, a set of blocks
+
+    Returns:
+      noise: numpy array with size [1, 256, 256, 3], updated noise
+      num_queries: int, query count
+      curr_loss: float, the value of loss function
+      success: boolean, True if attack is successful
+    """
     # Local variables
     priority_queue = []
     num_queries = 0
     
-    # Setting
+    # Check if block are in the working set
     A = np.zeros([len(blocks)], np.int32)
     for i, block in enumerate(blocks):
       upper_left, _, channel = block
@@ -54,14 +131,20 @@ class LazyLocalSearchHelper(object):
       feed_dict={self.x_input: image_batch, self.y_input: label_batch})
     num_queries += 1
     curr_loss = losses[0]
-      
-    if preds != label:
-      return noise, num_queries, curr_loss, True
-
+    
+    # Early stopping
+    if self.targeted:
+      if preds == label:
+        return noise, num_queries, curr_loss, True
+    else:
+      if preds != label:
+        return noise, num_queries, curr_loss, True
+    
+    # Main loop
     for _ in range(self.max_iters):
-      # First forward passes
+      # Lazy Greedy Insert
       indices,  = np.where(A==0)
-      
+
       batch_size = 100
       num_batches = int(math.ceil(len(indices)/batch_size))
       
@@ -81,7 +164,7 @@ class LazyLocalSearchHelper(object):
           feed_dict={self.x_input: image_batch, self.y_input: label_batch})
         
         # Early stopping
-        success_indices, = np.where(preds != label)
+        success_indices, = np.where(preds == label) if self.targeted else np.where(preds != label)
         if len(success_indices) > 0:
           noise[0, ...] = noise_batch[success_indices[0], ...]
           curr_loss = losses[success_indices[0]]
@@ -95,15 +178,16 @@ class LazyLocalSearchHelper(object):
           margin = losses[i]-curr_loss
           heapq.heappush(priority_queue, (margin, idx))
       
-      # Pick the best element and perturb the image   
-      best_margin, best_idx = heapq.heappop(priority_queue)
-      curr_loss += best_margin
-      noise = self._flip_noise(noise, blocks[best_idx])
-      A[best_idx] = 1
+      # Pick the best element and insert it into working set   
+      if len(priority_queue) > 0:
+        best_margin, best_idx = heapq.heappop(priority_queue)
+        curr_loss += best_margin
+        noise = self._flip_noise(noise, blocks[best_idx])
+        A[best_idx] = 1
       
-      # Add element into the set
+      # Add elements into working set
       while len(priority_queue) > 0:
-        # pick the best element
+        # Pick the best element
         cand_margin, cand_idx = heapq.heappop(priority_queue)
         
         # Re-evalulate the element
@@ -116,25 +200,29 @@ class LazyLocalSearchHelper(object):
         num_queries += 1
         margin = losses[0]-curr_loss
         
-        # If the cardinality has not changed, perturb the image
+        # If the cardinality has not changed, add the element
         if len(priority_queue) == 0 or margin <= priority_queue[0][0]:
           # If there is no element that has negative margin, then break
-          if margin >= 0:
+          if margin > 0:
             break
           # Perturb image
           curr_loss = losses[0]
           noise = self._flip_noise(noise, blocks[cand_idx])
           A[cand_idx] = 1
           # Early stopping
-          if preds != label:
-            return noise, num_queries, curr_loss, True
+          if self.targeted:
+            if preds == label:
+              return noise, num_queries, curr_loss, True
+          else: 
+            if preds != label:
+              return noise, num_queries, curr_loss, True
         # If the cardinality has changed, push the element into the priority queue
         else:
           heapq.heappush(priority_queue, (margin, cand_idx))
 	    
       priority_queue = []
 
-      # Now delete element
+      # Lazy Greedy Delete
       indices,  = np.where(A==1)
        
       batch_size = 100
@@ -156,7 +244,7 @@ class LazyLocalSearchHelper(object):
           feed_dict={self.x_input: image_batch, self.y_input: label_batch})
         
         # Early stopping
-        success_indices, = np.where(preds != label)
+        success_indices, = np.where(preds == label) if self.targeted else np.where(preds != label)
         if len(success_indices) > 0:
           noise[0, ...] = noise_batch[success_indices[0], ...]
           curr_loss = losses[success_indices[0]]
@@ -170,13 +258,14 @@ class LazyLocalSearchHelper(object):
           margin = losses[i]-curr_loss
           heapq.heappush(priority_queue, (margin, idx))
     
-      # Pick the best element and perturb the image   
-      best_margin, best_idx = heapq.heappop(priority_queue)
-      curr_loss += best_margin
-      noise = self._flip_noise(noise, blocks[best_idx])
-      A[best_idx] = 0
+      # Pick the best element and remove it from working set   
+      if len(priority_queue) > 0:
+        best_margin, best_idx = heapq.heappop(priority_queue)
+        curr_loss += best_margin
+        noise = self._flip_noise(noise, blocks[best_idx])
+        A[best_idx] = 0
       
-      # Add element into the set
+      # Delete elements from working set
       while len(priority_queue) > 0:
         # pick the best element
         cand_margin, cand_idx = heapq.heappop(priority_queue)
@@ -190,18 +279,22 @@ class LazyLocalSearchHelper(object):
         num_queries += 1 
         margin = losses[0]-curr_loss
       
-        # If the cardinality has not changed, perturb the image
+        # If the cardinality has not changed, remove the element
         if len(priority_queue) == 0 or margin <= priority_queue[0][0]:
           # If there is no element that has negative margin, then break
-          if margin >= 0:
+          if margin > 0:
             break
-          # Perturb image
+          # Update noise
           curr_loss = losses[0]
           noise = self._flip_noise(noise, blocks[cand_idx])
           A[cand_idx] = 0
           # Early stopping
-          if preds != label:
-            return noise, num_queries, curr_loss, True
+          if self.targeted:
+             if preds == label:
+              return noise, num_queries, curr_loss, True
+          else: 
+            if preds != label:
+              return noise, num_queries, curr_loss, True
         # If the cardinality has changed, push the element into the priority queue
         else:
           heapq.heappush(priority_queue, (margin, cand_idx))
