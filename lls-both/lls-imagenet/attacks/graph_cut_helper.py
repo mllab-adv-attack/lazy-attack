114bd1c31c1dfb4a168ef26bcdfe3739b2ade8bc
drillermoon <symoon11@snu.ac.kr>
2019-05-14 17:41:55 +0900

    sym

--- lls-both/lls-imagenet/attacks/graph_cut_helper.py
+++ lls-both/lls-imagenet/attacks/graph_cut_helper.py
@@ -1,22 +1,62 @@
-import numpy
+import itertools
+import numpy as np
 import ortools.graph.pywrapgraph import SimpleMaxFlow
 
 
 class GraphCutHelper(object):
-  def __init__(self):
-    pass
+  def __init__(self, args):
+    self.alpha = args.alpha
+    self.beta = args.beta
+  
+  
+  
 
-  def create_graph(self, edges):
-    start_nodes = []
-    end_nodes = []
-    capacities = []
+  def _find_neighbors(self, x, y):
+    height = self.height
+    width = self.width
+    radius = self.radius
+    hs = np.arange(max(x-radius, 0), min(x+radius, height))
+    ws = np.arange(max(y-radius, 0), min(y+radius, width))
+    
+    return itertools.product(hs, ys): 
 
-    self.graph = (start_nodes, end_nodes, capacities)
+  def create_graph(self, mask):
+    self.height, self.width = np.shape(mask)
+    num_nodes = self.height*self.width
+    start_index = num_nodes
+    end_index = num_nodes+1
+    edges = {}
+    
+    xs, ys = np.where(mask==1)
+    for x, y in zip(xs, ys): 
+      index = x*self.width+y
+      
+      # unary
+      edges[(start_index, index)] = 0
+      edges[(index, end_index)] = 0
+      
+      # pairwise 
+      neighbors = self._find_neighbors(x, y)      
+      for neighbor in neighbors:   
+        h, w = neighbor
+        index_neighbor = h*self.width+w
+        
+        # this node need to be fixed
+        if mask[h, w] == 0:
+          edges[(start_index, index_neighbor)] = 0
+          edges[(index_neighbor, end_index)] = 0
+        
+        edges[(index, index_neighbor)] = self.beta
+        edges[(index_neighbor, index)] = self.beta
 
+    self.graph = edges
+    
   def solve(self, source, sink):
-    start_nodes, end_nodes, capacities = self.graph
+    edges = self.graph
     max_flow = SimpleMaxFlow()
     
-    for start, end, capacity in zip(start_nodes, end_nodes, capacities):
+    for key, val in edges.items():
+      start, end = key
+      capacity = val
       max_flow.AddArcWithCapacity(start, end, capacity)
 
