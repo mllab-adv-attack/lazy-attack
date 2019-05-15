b0e065ee96c44af4805d561a5aa6f9629cb05a96
drillermoon <symoon11@snu.ac.kr>
2019-05-15 13:12:11 +0900

    sym

--- lls-both/lls-imagenet/attacks/graph_cut_helper.py
+++ lls-both/lls-imagenet/attacks/graph_cut_helper.py
@@ -17,22 +17,27 @@ class GraphCutHelper(object):
     
     return itertools.product(hs, ws)
 
-  def create_graph(self, mask):
+  def create_graph(self, mask, noise, latest_gain):
     self.height, self.width = np.shape(mask)
     num_nodes = self.height*self.width
-    self.start_index = num_nodes
-    self.end_index = num_nodes+1
+    self.source_index = num_nodes
+    self.sink_index = num_nodes+1
     edges = {}
     
     xs, ys = np.where(mask==1)
     for x, y in zip(xs, ys): 
       index = x*self.width+y
+      gain = latest_gain[x, y]
       
-      # unary
-      edges[(self.start_index, index)] = 0
-      edges[(index, self.end_index)] = 0
+      """Should be fixed, check if gain is positive or negative"""
+      source_capacity = 0
+      sink_capacity = 0
+
+      # unary term
+      edges[(self.source_index, index)] = int(self.alpha*source_capacity)
+      edges[(index, self.sink_index)] = int(self.alpha*sink_capacity)
       
-      # pairwise 
+      # pairwise term
       neighbors = self._find_neighbors(x, y)      
       for neighbor in neighbors:   
         h, w = neighbor
@@ -40,8 +45,12 @@ class GraphCutHelper(object):
         
         # this node need to be fixed
         if mask[h, w] == 0:
-          edges[(self.start_index, index_neighbor)] = 0
-          edges[(index_neighbor, self.end_index)] = 0
+          """Should be fixed, check the sign of noise""" 
+          source_capacity = 0 
+          sink_capacity = 0
+           
+          edges[(self.source_index, index_neighbor)] = int(self.alpha*source_capacity)
+          edges[(index_neighbor, self.sink_index)] = int(self.alpha*sink_capacity)
         
         edges[(index, index_neighbor)] = self.beta
         edges[(index_neighbor, index)] = self.beta
@@ -57,21 +66,21 @@ class GraphCutHelper(object):
       capacity = val
       max_flow.AddArcWithCapacity(start, end, capacity)
     
-    if max_flow.Solve(self.start_index, self.end_index) == max_flow.OPTIMAL:
+    if max_flow.Solve(self.source_index, self.sink_index) == max_flow.OPTIMAL:
       source = max_flow.GetSourceSideMinCut()
       sink = max_flow.GetSinkSideMinCut()
       
       assignment = np.zeros([self.height, self.width], dtype=np.int32)
       
       for idx in source:
-        if idx == self.start_index:
+        if idx == self.source_index:
           continue
         h = idx // self.width
         w = idx % self.width
         assignment[h, w] = -1
       
       for idx in sink:
-        if idx == self.end_index:
+        if idx == self.sink_index:
           continue
         h = idx // self.width
         w = idx % self.width
