da047b470cd0dea6b06cb12f8d067a58e347487b
dssrgu <white0234@snu.ac.kr>
2019-06-29 20:48:40 +0900

    goa: first commit

--- gaon_cifar/impenetrable.py
+++ gaon_cifar/impenetrable.py
@@ -29,57 +29,6 @@ if __name__ == '__main__':
 
 import cifar10_input
 
-class LinfPGDAttack:
-  def __init__(self, model, epsilon, num_steps, step_size, random_start, loss_func):
-    """Attack parameter initialization. The attack performs k steps of
-    size a, while always staying within epsilon from the initial
-       point."""
-    self.model = model
-    self.epsilon = epsilon
-    self.num_steps = num_steps
-    #self.step_size = step_size
-    self.step_size = step_size
-    self.rand = random_start
-    self.queries = []
-
-    if loss_func == 'xent':
-      self.loss = model.xent
-    elif loss_func == 'cw':
-      label_mask = tf.one_hot(model.y_input,
-                              10,
-                              on_value=1.0,
-                              off_value=0.0,
-                              dtype=tf.float32)
-      correct_logit = tf.reduce_sum(label_mask * model.pre_softmax, axis=1)
-      wrong_logit = tf.reduce_max((1-label_mask) * model.pre_softmax - 1e4*label_mask, axis=1)
-      self.loss = -tf.nn.relu(correct_logit - wrong_logit + 50)
-    else:
-      print('Unknown loss function. Defaulting to cross-entropy')
-      self.loss = model.xent
-
-    self.grad = tf.gradients(self.loss, model.x_input)[0]
-    
-  def perturb(self, x_nat, y, sess):
-
-    """Given a set of examples (x_nat, y), returns a set of adversarial
-    examples within epsilon of x_nat in l_infinity norm."""
-    if self.rand:
-      x = x_nat + np.random.uniform(-self.epsilon, self.epsilon, x_nat.shape)
-      x = np.clip(x, 0, 255)
-    else:
-      x = np.copy(x_nat)
-
-    for i in range(self.num_steps):
-      grad = sess.run(self.grad, feed_dict={self.model.x_input: x,
-                                              self.model.y_input: y})
-
-      x += self.step_size * np.sign(grad)
-
-      x = np.clip(x, x_nat - self.epsilon, x_nat + self.epsilon)
-      x = np.clip(x, 0, 255) # ensure valid pixel range
-
-    return x
-
 def run_attack(x_adv, model, sess, x_full_batch, y_full_batch):
 
   num_eval_samples = x_adv.shape[0]
