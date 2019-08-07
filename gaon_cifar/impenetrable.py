f81a976054e129bb61c133fb5b2b240a2d8ba49f
dssrgu <white0234@snu.ac.kr>
2019-08-07 17:08:19 +0900

    goa

--- gaon_cifar/impenetrable.py
+++ gaon_cifar/impenetrable.py
@@ -322,7 +322,7 @@ if __name__ == '__main__':
     parser.add_argument('--imp_random_start', action='store_true')
     parser.add_argument('--imp_gray_start', action='store_true')
     parser.add_argument('--imp_num_steps', default=1000, help='0 for until convergence', type=int)
-    parser.add_argument('--res_num_steps', default=1, type=int)
+    parser.add_argument('--res_num_steps', default=0, type=int)
     parser.add_argument('--res_step_size', default=1, type=int)
     # evaluation
     parser.add_argument('--val_step', default=100, help="validation per val_step iterations. =< 0 means no evaluation", type=int)
@@ -363,7 +363,7 @@ if __name__ == '__main__':
         saver.restore(sess, model_file)
 
         # Iterate over the samples batch-by-batch
-        num_eval_examples = params.sample_size + params.bstart
+        num_eval_examples = params.sample_size
         if params.val_step > 0:
             eval_batch_size = 1
         else:
@@ -371,17 +371,15 @@ if __name__ == '__main__':
 
         num_batches = int(math.ceil(num_eval_examples / eval_batch_size))
 
-        x_org = []  # org accumulator
         x_imp = []  # imp accumulator
         x_adv = []  # adv accumulator
-        y_imp = []  # y accumulator
 
-        if params.model_dir=='naturally_trained':
+        if params.model_dir == 'naturally_trained':
             indices = np.load('/data/home/gaon/lazy-attack/cifar10_data/nat_indices_untargeted.npy')
         else:
             indices = np.load('/data/home/gaon/lazy-attack/cifar10_data/indices_untargeted.npy')
 
-        bstart = 0
+        bstart = params.bstart
         while (True):
             x_candid = cifar.eval_data.xs[indices[bstart:bstart + 100]]
             y_candid = cifar.eval_data.ys[indices[bstart:bstart + 100]]
@@ -405,86 +403,81 @@ if __name__ == '__main__':
 
         x_full_batch = x_full_batch.astype(np.float32)
 
-        assert params.bstart % eval_batch_size == 0
-
         for ibatch in range(num_batches):
-            if ibatch * eval_batch_size >= params.bstart:
-                bstart = ibatch * eval_batch_size
-                bend = min(bstart + eval_batch_size, num_eval_examples)
-                print('batch size: {}'.format(bend - bstart))
+            bstart = ibatch * eval_batch_size
+            bend = min(bstart + eval_batch_size, num_eval_examples)
+            print('batch size: {}'.format(bend - bstart))
 
-                x_batch = x_full_batch[bstart:bend, :]
-                y_batch = y_full_batch[bstart:bend]
+            x_batch = x_full_batch[bstart:bend, :]
+            y_batch = y_full_batch[bstart:bend]
 
-                print('fortifying image ', bstart)
-                x_batch_imp = impenet.fortify(x_batch, y_batch, ibatch, meta_name, sess)
+            print('fortifying image ', bstart)
+            x_batch_imp = impenet.fortify(x_batch, y_batch, ibatch, meta_name, sess)
 
-                # evaluation
-                x_batch_adv = np.copy(x_batch_imp)
+            # evaluation
+            x_batch_adv = np.copy(x_batch_imp)
 
-                val_eps = 8
-                obj_eps = 8
-                num_images = len(x_batch_imp)
+            val_eps = 8
+            obj_eps = 8
+            num_images = len(x_batch_imp)
 
-                impenet.pgd.num_steps = 20
+            impenet.pgd.num_steps = 20
 
-                success_mask = [True for _ in range(num_images)]
+            success_mask = [True for _ in range(num_images)]
 
-                while val_eps <= obj_eps:
-                    val_iter = impenet.val_num
-                    val_total_corr = 0
+            while val_eps <= obj_eps:
+                val_iter = impenet.val_num
+                val_total_corr = 0
 
-                    impenet.pgd.epsilon = val_eps
-                    impenet.pgd.step_size = impenet.pgd.epsilon//4
+                impenet.pgd.epsilon = val_eps
+                impenet.pgd.step_size = impenet.pgd.epsilon//4
 
-                    for i in range(val_iter):
+                for i in range(val_iter):
 
-                        x_batch_adv = impenet.pgd.perturb(x_batch_imp, y_batch, sess,
-                                                          proj=True, reverse=False, rand=True)
+                    x_batch_adv = impenet.pgd.perturb(x_batch_imp, y_batch, sess,
+                                                      proj=True, reverse=False, rand=True)
 
-                        corr_mask = sess.run(impenet.model.correct_prediction,
-                                                       feed_dict={impenet.model.x_input: x_batch_adv,
-                                                                  impenet.model.y_input: y_batch})
+                    corr_mask = sess.run(impenet.model.correct_prediction,
+                                                   feed_dict={impenet.model.x_input: x_batch_adv,
+                                                              impenet.model.y_input: y_batch})
 
-                        success_mask *= corr_mask
-                        num_survived = np.sum(success_mask) / len(success_mask)
+                    success_mask *= corr_mask
+                    num_survived = np.sum(success_mask) / len(success_mask)
 
-                    print("{} validation accuracy: {:.2f}%".format(val_eps, num_survived * 100))
+                    if sum(success_mask) == 0:
+                        break
 
-                    # goal achievement check
-                    if num_survived >= 1:
-                        print("reached performance goal for", val_eps)
+                print("{} validation accuracy: {:.2f}%".format(val_eps, num_survived * 100))
 
-                        if val_eps == obj_eps:
-                            print("reached final objective!")
-                            break
-                    else:
-                        print("failed for", val_eps)
+                # goal achievement check
+                if num_survived >= 1:
+                    print("reached performance goal for", val_eps)
 
-                    val_eps += 1
-                
-                impenet.pgd.epsilon = impenet.eps
-                impenet.pgd.step_size = impenet.pgd_step_size
-                impenet.pgd.num_steps = impenet.pgd_num_steps
-                
+                    if val_eps == obj_eps:
+                        print("reached final objective!")
+                        break
+                else:
+                    print("failed for", val_eps)
 
-                x_org.append(x_batch)
-                x_imp.append(x_batch_imp)
-                x_adv.append(x_batch_adv)
-                y_imp.append(y_batch)
+                val_eps += 1
 
-        x_org = np.concatenate(x_org)
+            impenet.pgd.epsilon = impenet.eps
+            impenet.pgd.step_size = impenet.pgd_step_size
+            impenet.pgd.num_steps = impenet.pgd_num_steps
+
+            x_imp.append(x_batch_imp)
+            x_adv.append(x_batch_adv)
+
         x_imp = np.concatenate(x_imp)
         x_adv = np.concatenate(x_adv)
-        y_imp = np.concatenate(y_imp)
 
         folder_name = './arr' + '_main' + '/'
         batch_name = '_' + str(params.bstart) + '_' + str(params.sample_size)
         common_name = folder_name + meta_name + batch_name
         
-        np.save(common_name + '_x_org', x_org)
+        np.save(common_name + '_x_org', x_full_batch)
         np.save(common_name + '_x_imp', x_imp)
-        np.save(common_name + '_y', y_imp)
+        np.save(common_name + '_y', y_full_batch)
 
         if np.amax(x_imp) > 255.0001 or \
             np.amin(x_imp) < -0.0001 or \
@@ -497,6 +490,6 @@ if __name__ == '__main__':
             print('Invalid pixel range in x_adv. Expected [0,255], fount[{},{}]'.format(np.amin(x_adv),
                                                                                         np.amax(x_adv)))
         else:
-            result(x_imp, x_adv, model, sess, x_org, y_imp)
+            result(x_imp, x_adv, model, sess, x_full_batch, y_full_batch)
 
 
