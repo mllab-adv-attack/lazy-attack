bf33a8d42ac0e4c85b816c1a9ba817c9f8b090b5
dssrgu <white0234@snu.ac.kr>
2019-09-27 23:44:47 +0900

    goa

--- gaon_cifar/infer_eval.py
+++ gaon_cifar/infer_eval.py
@@ -15,8 +15,8 @@ import numpy as np
 
 import cifar10_input
 
-from infer_model import Model as Safe_model
-from infer_target import Model as Target_model
+from infer_model import generator as Generator
+from model import Model as Model
 
 from utils import infer_file_name
 
@@ -72,8 +72,24 @@ eval_batch_size = args.eval_batch_size
 raw_cifar = cifar10_input.CIFAR10Data(data_path)
 global_step = tf.train.get_or_create_global_step()
 
-model = Target_model('eval')
-full_model = Safe_model('eval', model, args)
+model = Model('eval')
+
+x_input = tf.placeholder(
+    tf.float32,
+    shape=[None, 32, 32, 3]
+)
+y_input = tf.placeholder(
+    tf.int64,
+    shape=None
+)
+generator = tf.make_template('generator', Generator, f_dim=64, output_size=32, c_dim=3, is_training=False)
+
+pgd = LinfPGDAttack(model,
+                    args.eps,
+                    args.num_steps,
+                    args.step_size,
+                    True,
+                    'xent')
 
 # Setting up the optimizer
 boundaries = [0, 40000, 60000]
@@ -83,13 +99,6 @@ learning_rate = tf.train.piecewise_constant(
     tf.cast(global_step, tf.int32),
     boundaries,
     values)
-
-# set up metrics
-total_loss = full_model.safe_pgd_mean_xent
-safe_pgd_acc = full_model.safe_pgd_accuracy
-orig_acc = full_model.orig_accuracy
-safe_acc = full_model.safe_accuracy
-l2_dist = tf.reduce_mean(tf.norm((full_model.x_safe-full_model.x_input)/255, axis=0))
 
 # Setting up the Tensorboard and checkpoint outputs
 meta_name = infer_file_name(args)
@@ -173,14 +182,12 @@ with tf.Session() as sess:
 
         x_full_batch = x_full_batch.astype(np.float32)
 
-        x_imp = []  # imp accumulator
-        step_imp = []  # num steps accumulator
-        l2_li = [] # l2 distance accumulator
+        full_mask = []
 
         for ibatch in range(num_batches):
             bstart = ibatch * eval_batch_size
             bend = min(bstart + eval_batch_size, num_eval_examples)
-            print('batch size: {}'.format(bend - bstart))
+            print('batch {} - {}'.format(bstart, bend-1))
 
             x_batch = x_full_batch[bstart:bend, :]
             y_batch = y_full_batch[bstart:bend]
@@ -189,186 +196,20 @@ with tf.Session() as sess:
 
             for _ in range(args.restarts):
                 
-                cur_corr = sess.sun(
+                x_batch_attacked = pgd.perturb(x_batch, y_batch, sess,
+                                       proj=True, reverse=False, rand=True)
 
+                correct_prediction = sess.sun(model.correct_prediction,
+                                               feed_dict={model.x_input: x_batch_attacked,
+                                                          model.y_input: y_batch})
 
-            # run our algorithm
-            print('fortifying image ', bstart)
-            x_batch_imp, step_batch_imp = impenet.fortify(x_batch, y_batch, sess, y_batch_gt)
-            print()
+                mask *= correct_prediction
 
-            # evaluation
-            # y_batch_hard = np.argmax(y_batch, axis=1)
-            # y_batch_hard = np.eye(10)[y_batch_hard.reshape(-1)]
+            full_mask.append(mask)
 
-            # suc_flag = impenet.validation(x_batch_imp, y_batch, y_batch_hard)
+            print('{}/{} safe'.format(np.sum(mask), np.size(mask)))
 
-            x_imp.append(x_batch_imp)
-            step_imp.append(np.array([step_batch_imp]))
-            l2_li.append(np.linalg.norm((x_batch_imp - x_batch)/255))
-            print('l2 distance (curr):', np.linalg.norm(x_batch_imp - x_batch)/255)
-            print('l2 distance (total):', np.mean(l2_li))
-            print()
-            print('------------------------------------------------------------------------------')
-            print('------------------------------------------------------------------------------')
+        full_mask = np.concatenate(full_mask)
 
-        x_imp = np.concatenate(x_imp)
-        step_imp = np.concatenate(step_imp)
+        print("evaluation accuracy: {:.2f}".format(np.mean(full_mask)*100))
 
-        # save image
-        folder_name = './arr' + '_main' + '/'
-        batch_name = '_' + str(args.bstart) + '_' + str(args.sample_size)
-        common_name = folder_name + meta_name + batch_name
-        
-        np.save(common_name + '_x_org', x_full_batch)
-        np.save(common_name + '_x_imp', x_imp)
-        np.save(common_name + '_step_imp', step_imp)
-        np.save(common_name + '_y', y_full_batch)
-
-        # sanity check
-        if np.amax(x_imp) > 255.0001 or \
-            np.amin(x_imp) < -0.0001 or \
-            np.isnan(np.amax(x_imp)):
-            print('Invalid pixel range in x_imp. Expected [0,255], fount[{},{}]'.format(np.amin(x_imp),
-                                                                                        np.amax(x_imp)))
-        else:
-            result(x_imp, model, sess, x_full_batch, y_full_batch)
-
-
-
-    # Main training loop
-    for ii in range(max_num_training_steps):
-        x_batch, y_batch = cifar.train_data.get_next_batch(training_batch_size,
-                                                           multiple_passes=True)
-
-        # Actual training step
-        nat_dict = {full_model.x_input: x_batch,
-                    full_model.y_input: y_batch}
-
-        assert 0 <= np.amin(x_batch) and np.amax(x_batch) <= 255.0
-
-        start = timer()
-        _, total_loss_batch, safe_pgd_acc_batch, orig_acc_batch, safe_acc_batch, \
-        x_safe, x_safe_pgd, l2_dist_batch, train_merged_summaries_batch = \
-            sess.run([train_step, total_loss, safe_pgd_acc, orig_acc, safe_acc,
-                      full_model.x_safe, full_model.x_safe_pgd, l2_dist, train_merged_summaries], feed_dict=nat_dict)
-        end = timer()
-
-        assert 0 <= np.amin(x_safe) and np.amax(x_safe) <= 255.0
-        assert 0 <= np.amin(x_safe_pgd) and np.amax(x_safe_pgd) <= 255.0
-
-        training_time += end - start
-
-        # Output to stdout
-        if ii % num_output_steps == 0:
-            print('Step {}:    ({})'.format(ii, datetime.now()))
-            print('    orig accuracy {:.4}%'.format(orig_acc_batch * 100))
-            print('    safe accuracy {:.4}%'.format(safe_acc_batch * 100))
-            print('    safe_pgd accuracy {:.4}%'.format(safe_pgd_acc_batch * 100))
-            print('    l2 dist {:.4}'.format(l2_dist_batch))
-            print('    total loss {:.6}'.format(total_loss_batch))
-            if ii != 0:
-                print('    {} examples per second'.format(
-                    num_output_steps * training_batch_size / training_time))
-                training_time = 0.0
-
-        # Tensorboard summaries
-        if ii % num_summary_steps == 0:
-            if not args.no_save:
-                summary_writer.add_summary(train_merged_summaries_batch, global_step.eval(sess))
-            # evaluate on test set
-            eval_bstart = (ii//num_summary_steps)*eval_batch_size
-            eval_bend = (ii//num_summary_steps+1)*eval_batch_size
-
-            eval_x_batch = raw_cifar.eval_data.xs[eval_indice[eval_bstart:eval_bend]]
-            eval_y_batch = raw_cifar.eval_data.ys[eval_indice[eval_bstart:eval_bend]]
-            eval_dict = {full_model.x_input: eval_x_batch,
-                         full_model.y_input: eval_y_batch}
-            total_loss_batch, safe_pgd_acc_batch, orig_acc_batch, safe_acc_batch, \
-                x_safe, x_safe_pgd, l2_dist_batch, eval_merged_summaries_batch = \
-                sess.run([total_loss, safe_pgd_acc, orig_acc, safe_acc,
-                          full_model.x_safe, full_model.x_safe_pgd, l2_dist, eval_merged_summaries], feed_dict=eval_dict)
-            
-            print('    orig accuracy (eval) {:.4}%'.format(orig_acc_batch * 100))
-            print('    safe accuracy (eval) {:.4}%'.format(safe_acc_batch * 100))
-            print('    safe_pgd accuracy (eval) {:.4}%'.format(safe_pgd_acc_batch * 100))
-            print('    l2 dist (eval) {:.4}'.format(l2_dist_batch))
-            print('    total loss (eval) {:.6}'.format(total_loss_batch))
-            
-            if not args.no_save:
-                summary_writer.add_summary(eval_merged_summaries_batch, global_step.eval(sess))
-
-
-        # Write a checkpoint
-        if ii % num_checkpoint_steps == 0 and not args.no_save:
-            saver.save(sess,
-                       os.path.join(save_dir, 'checkpoint'),
-                       global_step=global_step)
-    # Main training loop
-    for ii in range(max_num_training_steps):
-        x_batch, y_batch = cifar.train_data.get_next_batch(training_batch_size,
-                                                           multiple_passes=True)
-
-        # Actual training step
-        nat_dict = {full_model.x_input: x_batch,
-                    full_model.y_input: y_batch}
-
-        assert 0 <= np.amin(x_batch) and np.amax(x_batch) <= 255.0
-
-        start = timer()
-        _, total_loss_batch, safe_pgd_acc_batch, orig_acc_batch, safe_acc_batch, \
-        x_safe, x_safe_pgd, l2_dist_batch, train_merged_summaries_batch = \
-            sess.run([train_step, total_loss, safe_pgd_acc, orig_acc, safe_acc,
-                      full_model.x_safe, full_model.x_safe_pgd, l2_dist, train_merged_summaries], feed_dict=nat_dict)
-        end = timer()
-
-        assert 0 <= np.amin(x_safe) and np.amax(x_safe) <= 255.0
-        assert 0 <= np.amin(x_safe_pgd) and np.amax(x_safe_pgd) <= 255.0
-
-        training_time += end - start
-
-        # Output to stdout
-        if ii % num_output_steps == 0:
-            print('Step {}:    ({})'.format(ii, datetime.now()))
-            print('    orig accuracy {:.4}%'.format(orig_acc_batch * 100))
-            print('    safe accuracy {:.4}%'.format(safe_acc_batch * 100))
-            print('    safe_pgd accuracy {:.4}%'.format(safe_pgd_acc_batch * 100))
-            print('    l2 dist {:.4}'.format(l2_dist_batch))
-            print('    total loss {:.6}'.format(total_loss_batch))
-            if ii != 0:
-                print('    {} examples per second'.format(
-                    num_output_steps * training_batch_size / training_time))
-                training_time = 0.0
-
-        # Tensorboard summaries
-        if ii % num_summary_steps == 0:
-            if not args.no_save:
-                summary_writer.add_summary(train_merged_summaries_batch, global_step.eval(sess))
-            # evaluate on test set
-            eval_bstart = (ii//num_summary_steps)*eval_batch_size
-            eval_bend = (ii//num_summary_steps+1)*eval_batch_size
-
-            eval_x_batch = raw_cifar.eval_data.xs[eval_indice[eval_bstart:eval_bend]]
-            eval_y_batch = raw_cifar.eval_data.ys[eval_indice[eval_bstart:eval_bend]]
-            eval_dict = {full_model.x_input: eval_x_batch,
-                         full_model.y_input: eval_y_batch}
-            total_loss_batch, safe_pgd_acc_batch, orig_acc_batch, safe_acc_batch, \
-                x_safe, x_safe_pgd, l2_dist_batch, eval_merged_summaries_batch = \
-                sess.run([total_loss, safe_pgd_acc, orig_acc, safe_acc,
-                          full_model.x_safe, full_model.x_safe_pgd, l2_dist, eval_merged_summaries], feed_dict=eval_dict)
-            
-            print('    orig accuracy (eval) {:.4}%'.format(orig_acc_batch * 100))
-            print('    safe accuracy (eval) {:.4}%'.format(safe_acc_batch * 100))
-            print('    safe_pgd accuracy (eval) {:.4}%'.format(safe_pgd_acc_batch * 100))
-            print('    l2 dist (eval) {:.4}'.format(l2_dist_batch))
-            print('    total loss (eval) {:.6}'.format(total_loss_batch))
-            
-            if not args.no_save:
-                summary_writer.add_summary(eval_merged_summaries_batch, global_step.eval(sess))
-
-
-        # Write a checkpoint
-        if ii % num_checkpoint_steps == 0 and not args.no_save:
-            saver.save(sess,
-                       os.path.join(save_dir, 'checkpoint'),
-                       global_step=global_step)
