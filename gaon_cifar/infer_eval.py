729824cfe383e03210a76212900b20fa01760bb3
dssrgu <white0234@snu.ac.kr>
2019-09-28 01:23:41 +0900

    goa

--- gaon_cifar/infer_eval.py
+++ gaon_cifar/infer_eval.py
@@ -120,7 +120,31 @@ if model_file is None:
 
 with tf.Session() as sess:
 
-    saver.restore(sess, model_file)
+    # Restore variables if can, set optimizer
+    reader = tf.train.NewCheckpointReader(model_file)
+    saved_shapes = reader.get_variable_to_shape_map()
+    var_names = sorted([(var.name, var.name.split(':')[0]) for var in tf.global_variables()
+                        if var.name.split(':')[0] in saved_shapes])
+    restore_vars = []
+    restore_vars_name_list = []
+    for var_name, saved_var_name in var_names:
+        curr_var = tf.get_default_graph().get_tensor_by_name(var_name)
+        var_shape = curr_var.get_shape().as_list()
+        if var_shape == saved_shapes[saved_var_name] and 'global_step' not in saved_var_name:
+            restore_vars.append(curr_var)
+            restore_vars_name_list.append(saved_var_name + ':0')
+
+    trainable_variables = tf.trainable_variables()
+    variables_to_train = [var for var in trainable_variables if var.name not in restore_vars_name_list]
+
+    train_step = tf.train.AdamOptimizer(learning_rate).minimize(
+        total_loss,
+        global_step=global_step,
+        var_list=variables_to_train)
+
+    sess.run(tf.global_variables_initializer())
+    opt_saver = tf.train.Saver(restore_vars)
+    opt_saver.restore(sess, model_file)
     print('restore success!')
 
     num_eval_examples = args.sample_size
